735963492d71409bae8ac21f3732931d9ad0f0e0
andmatsou <andmatsou@gmail.com>
2022-05-31 12:31:32 -0300

    feat: refatorando e criando um botão dropdown na página listar

--- js/custom_adm.js
+++ js/custom_adm.js
@@ -24,3 +24,28 @@ let avatar = document.querySelector(".avatar"),
       })
 
    window.matchMedia("(max-width : 768px)").matches ? sidebar.classList.remove("active") : sidebar.classList.add("active");
+
+   //Botão ação do listar
+   function actionDropdown(id){
+      closeDropdownAction();
+      document.getElementById("actionDropdown" + id).classList.toggle("show-dropdown-action");
+   }
+
+   window.onclick = function(event) {
+      if(!event.target.matches('.dropdown-btn-action')) {
+         //document.getElementById("actionDropdown").classList.remove("show-dropdown-action");
+         closeDropdownAction();
+      }
+   }
+
+   function closeDropdownAction(){
+      const dropdowns = document.getElementsByClassName("dropdown-action-item");
+      
+      for (let i = 0; i < dropdowns.length; i++) {
+         let openDropdown = dropdowns[i];
+         if(openDropdown.classList.contains("show-dropdown-action")){
+            openDropdown.classList.remove("show-dropdown-action");
+         }
+         
+      }
+   } 
